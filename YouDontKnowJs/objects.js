ba87cee53d81e256ecb8ee95915af4929ebe299c
Dustin Stanley <dustin.stanley@gmail.com>
2016-01-18 22:22:42 -0800

    Add react shopping cart tutorial

--- YouDontKnowJs/objects.js
+++ YouDontKnowJs/objects.js
@@ -20,3 +20,19 @@ There are a few special object subtypes:
 
 var constructedString = new String("mahString");
 console.log(Object.prototype.toString.call(constructedString));	//[object String]
+
+//Objects are collections of key value pairs.  The values are accessed as properties, via the .propName or 
+//["propName"] syntax.  Whenever these are accessed, the JS engine will invoke an internal default [[Get]] or
+//[[Put]] operation which will look for the property on the object as well as traverse its prototype chain.
+
+//Object properties have attributes (called "descriptors") such as "writable" and "configurable".  Also, objects
+//can have their write behavior/mutability behavior controlled by functions like Object.preventExtensions(), Object.seal,
+//Object.freeze(), etc.
+
+//Properties can be optionally enumerable.  If a property is enumerable, it can be accessed via an iterator that
+//loops over the properties of an object.  Properties that are not enumerable will not be iterated over in
+//a loop (such as a "for..in" loop).
+
+//Do not use "for..in" to iterate over arrays as this will also iterate over any properties defined on the array
+//along with the indices in the array, which is almost always not the intended behavior and likely to cause bugs.
+//Use "for..in" with objects only.
