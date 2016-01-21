8656c9bbf82f33b526336aa1369e0b1e1f96944f
Dustin Stanley <dustin.stanley@gmail.com>
2016-01-21 01:25:43 -0800

    Add java and .net performance test

--- Java8Sandbox/src/test/java/Sandbox/LambdaExpressionsUnitTest.java
+++ Java8Sandbox/src/test/java/Sandbox/LambdaExpressionsUnitTest.java
@@ -1,11 +1,17 @@
 package Sandbox;
 
+import com.google.common.base.Stopwatch;
+import com.google.common.primitives.Doubles;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class LambdaExpressionsUnitTest {
 
@@ -48,5 +54,69 @@ public class LambdaExpressionsUnitTest {
 //							.sorted(comparing(Transaction::getValue).reversed())
 //							.map(Transaction::getId)
 //							.collect(toList());
+	}
+
+	@Test
+	public void streamPerformanceTest() throws Exception {
+		int sampleSize = 50000000;
+		double[] arrayToTest = new double[sampleSize];
+
+		for (int i = 0; i < sampleSize; i++) {
+			arrayToTest[i] = Math.random();
+		}
+
+		Stopwatch sw = new Stopwatch();
+
+		sw.start();
+		double result = Double.MIN_VALUE;
+		for (int i = 0; i < sampleSize; i++) {
+			if (arrayToTest[i] > result) {
+				result = arrayToTest[i];
+			}
+		}
+		sw.stop();
+		System.out.println("Array performance: " + sw.elapsedTime(TimeUnit.NANOSECONDS) / 1000000.00 +
+				" ms.  Final result: " + result);
+
+		sw.reset();
+		List<Double> list = Doubles.asList(arrayToTest);
+		sw.start();
+		result = Double.MIN_VALUE;
+		for (int i = 0; i < sampleSize; i++) {
+			if (list.get(i) > result) {
+				result = list.get(i);
+			}
+		}
+		sw.stop();
+		System.out.println("List performance: " + sw.elapsedTime(TimeUnit.NANOSECONDS) / 1000000.00 +
+				" ms.  Final result: " + result);
+
+		sw.reset();
+		sw.start();
+		result = Arrays.stream(arrayToTest).reduce(Double.MIN_VALUE, Math::max);
+		sw.stop();
+		System.out.println("Array sequential stream performance: " + sw.elapsedTime(TimeUnit.NANOSECONDS) / 1000000.00 +
+				" ms.  Final result: " + result);
+
+		sw.reset();
+		sw.start();
+		result = Arrays.stream(arrayToTest).parallel().reduce(Double.MIN_VALUE, Math::max);
+		sw.stop();
+		System.out.println("Array parallel stream performance: " + sw.elapsedTime(TimeUnit.NANOSECONDS) / 1000000.00 +
+				" ms.  Final result: " + result);
+
+		sw.reset();
+		sw.start();
+		result = list.stream().sequential().reduce(Double.MIN_VALUE, Math::max);
+		sw.stop();
+		System.out.println("List sequential stream performance: " + sw.elapsedTime(TimeUnit.NANOSECONDS) / 1000000.00 +
+				" ms.  Final result: " + result);
+
+		sw.reset();
+		sw.start();
+		result = list.stream().parallel().reduce(Double.MIN_VALUE, Math::max);
+		sw.stop();
+		System.out.println("List parallel stream performance: " + sw.elapsedTime(TimeUnit.NANOSECONDS) / 1000000.00 +
+				" ms.  Final result: " + result);
 	}
 }
---
+++ dotNet/ConsoleApplication2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sampleSize = 50000000;
+            double[] arrayToTest = new double[sampleSize];
+            Random rand = new System.Random();
+
+            for (int i = 0; i < sampleSize; i++)
+            {
+                arrayToTest[i] = rand.NextDouble();
+            }
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            double result = Double.MinValue;
+            for (int i = 0; i < sampleSize; i++)
+            {
+                if (arrayToTest[i] > result)
+                {
+                    result = arrayToTest[i];
+                }
+            }
+            sw.Stop();
+            System.Console.WriteLine("Array performance: " + sw.Elapsed.TotalMilliseconds +" ms.  Final result: " + result);
+
+            sw.Reset();
+            sw.Start();
+            result = Double.MinValue;
+            for (int i = 0; i < sampleSize; i++)
+            {
+                if (arrayToTest[i] > result)
+                {
+                    result = arrayToTest[i];
+                }
+            }
+            sw.Stop();
+            System.Console.WriteLine("Array performance 2nd run: " + sw.Elapsed.TotalMilliseconds + " ms.  Final result: " + result);
+
+            sw.Restart();
+            result = arrayToTest.Max();
+            sw.Stop();
+            System.Console.WriteLine("Array.Max() performance: " + sw.Elapsed.TotalMilliseconds + " ms.  Final result: " + result);
+
+            sw.Reset();
+            List<Double> list = arrayToTest.ToList<Double>();
+            sw.Start();
+            result = Double.MinValue;
+            for (int i = 0; i < sampleSize; i++)
+            {
+                if (list[i] > result)
+                {
+                    result = list[i];
+                }
+            }
+            sw.Stop();
+            System.Console.WriteLine("List performance: " + sw.Elapsed.TotalMilliseconds + " ms.  Final result: " + result);
+
+            sw.Restart();
+            result = list.Max();
+            sw.Stop();
+            System.Console.WriteLine("List.Max() performance: " + sw.Elapsed.TotalMilliseconds + " ms.  Final result: " + result);
+
+
+
+            System.Console.ReadLine();
+        }
+    }
+}
