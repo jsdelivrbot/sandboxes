1b9cb9de6b599aaddfbd50a03f97976c01674944
Dustin Stanley <dustin.stanley@gmail.com>
2016-02-21 18:58:05 -0800

    Continue full stack redux tutorial

--- reduxTutorial/voting-client/src/components/Voting.jsx
+++ reduxTutorial/voting-client/src/components/Voting.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
 
-export default React.createClass({
+
+export const Voting = React.createClass({
+  mixins: [PureRenderMixin],
   render: function () {
     return <div>
       {this.props.winner ? 
@@ -12,3 +16,14 @@ export default React.createClass({
     </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+connect(mapStateToProps)(Voting);
+
+export const VotingContainer = connect(mapStateToProps)(Voting);
