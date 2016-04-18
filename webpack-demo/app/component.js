0f3bdb51654ce194c41a23a24b8b1f8b4eaa1457
Dustin Stanley <dustin.stanley@gmail.com>
2016-04-18 00:08:58 -0700

    Add webpack tutorial project

--- webpack-demo/app/component.js
+++ webpack-demo/app/component.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     var element = document.createElement('h1');
     
-    element.innerHTML = 'Hello worldddddasdas.';
-    
+    element.innerHTML = 'Hello world!!!!';
+    console.log("HEL:LO WORLD!!!");
     return element;
 };
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,13 +1,21 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Load package.json so we can use 'depdendencies' from there
+const pkg = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
+    react: path.join(__dirname, 'node_modules/react/dist/react.min.js'),
     app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
+    build: path.join(__dirname, 'build'),
+    style: path.join(__dirname, 'app/main.css')
 };
 
 const common = {
@@ -15,35 +23,27 @@ const common = {
     //We'll be using the latter form given it's
     //convenient with more complex configurations.
     entry: {
-        app: PATHS.app
+        app: PATHS.app,
+        style: PATHS.style
     },
     output: {
         path: PATHS.build,
-        filename: 'bundle.js'
+        
+        //Output using the entry name
+        filename: '[name].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack demo'
         })
-    ],
-    module: {
-        loaders: [
-            {
-                // Test expects a RegExp! Note the slashes!
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                // Include accepts either a path or an array of paths.
-                include: PATHS.app
-            }
-        ]
-    }
+    ]
 };
 
 // Default configuration.  We will return this if
 // Webpack is called outside of npm.
 
 if (TARGET === 'start' || !TARGET) {
-    module.exports = merge(common, {                    
+    module.exports = merge(common, {
         devtool: 'eval-source-map',
         devServer: {
             // Enable history API fallback so HTML5 History API based
@@ -70,6 +70,16 @@ if (TARGET === 'start' || !TARGET) {
             // If you want defaults, you can use a little trick like this
             // port: process.env.PORT || 3000
         },
+        module: {
+            loaders: [
+                // Define development specific CSS setup
+                {
+                    test: /\.css$/,
+                    loaders: ['style', 'css'],
+                    include: PATHS.app
+                }
+            ]
+        },
         plugins: [
             new webpack.HotModuleReplacementPlugin(),
             new NpmInstallPlugin({
@@ -79,9 +89,41 @@ if (TARGET === 'start' || !TARGET) {
     });
 }
 
-if (TARGET === 'build') {
+if (TARGET === 'build' || TARGET === 'stats') {
     module.exports = merge(common, {
+        
+        // Define vendor entry point needed for splitting
+        entry: {
+          // Set up an entry chunk for our vendor bundle.
+          // You can filter out dependencies here if needed with
+          // '.filter(...)'
+          vendor: Object.keys(pkg.dependencies)  
+        },
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          chunkFilename: '[chunkhash].js'
+        },
+        module: {
+            loaders: [
+                // Extract CSS during build
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css'),
+                    include: PATHS.app
+                }
+            ]
+        },
         plugins: [
+            // Finds possible duplicate files/dependencies and removes them
+            new webpack.optimize.DedupePlugin(),
+            // Output extracted CSS to a file
+            new ExtractTextPlugin('[name].[chunkhash].css'),
+            new CleanWebpackPlugin([PATHS.build]),
+            // Extract vendor and manifest files
+            new webpack.optimize.CommonsChunkPlugin({
+               names: ['vendor', 'manifest']
+            }),
             // Setting DefinePlugin affects React library size!
             // DefinePlugin replaces content "as is" so we need some
             // extra quotes for the generated code to make sense
