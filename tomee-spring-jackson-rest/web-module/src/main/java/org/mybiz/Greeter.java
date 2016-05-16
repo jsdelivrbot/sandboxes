01facee2d34c86b6a6ffdb09aa54547571574088
Dustin Stanley <dustin.stanley@gmail.com>
2016-05-15 19:26:33 -0700

    Add TomEE rest project

---
+++ tomee-spring-jackson-rest/web-module/src/main/java/org/mybiz/Greeter.java
@@ -0,0 +1,13 @@
+package org.mybiz;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("greeting")
+public class Greeter {
+
+    @GET
+    public String sayHi() {
+        return "Hi!dfgdfgdfgdfghjghj1122g";
+    }
+}
