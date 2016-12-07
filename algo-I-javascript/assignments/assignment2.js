44a432a8a05b2dbc4604a0eba2e3a5bf4ab0e430
Dustin Stanley <dustin.stanley@gmail.com>
2016-12-07 00:05:08 -0800

    Algo MOOC - quicksort assignment

--- algo-I-javascript/assignments/assignment2.js
+++ algo-I-javascript/assignments/assignment2.js
@@ -1,44 +1,69 @@
+export const executeQuickSort = function(dataToSort, pivotFunction) {
+  return quickSort(dataToSort, 0, dataToSort.length - 1, pivotFunction);
+};
 
-
-export const quickSort = function(dataToSort, pivotFunction) {
+export const quickSort = function(dataToSort, l, r, choosePivotIndex) {
   let numComparisons = 0;
   if (dataToSort.length > 1) {
-    const pivot = pivotFunction(dataToSort);
-    //partition(dataToSort, pivot, dataToSort.length - 1);
-    //quickSort(firstPartOfData, pivotFunction);
-    //quickSort(secondPartOfData, pivotFunction);
+    const pivotIndex = choosePivotIndex(dataToSort, l, r);
+    const partitionedPivotIndex = partition(dataToSort, pivotIndex, l, r);
+
+    if (partitionedPivotIndex > l + 1) {
+      quickSort(dataToSort, l, partitionedPivotIndex - 1, choosePivotIndex);
+    }
+
+    if (partitionedPivotIndex < r) {
+      quickSort(dataToSort, partitionedPivotIndex + 1, r, choosePivotIndex);
+    }
   }
 
   return numComparisons;
 };
 
-export const pivotAroundFirstElement = function(dataToSort) {
-  return 0;
+export const pivotAroundFirstElement = function(dataToSort, l, r) {
+  return l;
 };
 
-export const pivotAroundLastElement = function(dataToSort) {
-  return dataToSort.length - 1;
+export const pivotAroundLastElement = function(dataToSort, l, r) {
+  return r;
 };
 
-export const pivotMedianOfThree = function(dataToSort) {
+export const pivotMedianOfThree = function(dataToSort, l, r) {
 
 };
 
-const partition = function(dataToPartition, l, r) {
-  const p = dataToPartition[l];
+/**
+ *
+ * @param dataToPartition
+ * @param p
+ * @param l
+ * @param r
+ * @returns {number}
+ */
+const partition = function(dataToPartition, p, l, r) {
+  const pivotValue = dataToPartition[p];
   let i = l + 1;
-  for (let j = l + 1; j < r; j++) {
-    if (dataToPartition[j] < p) {
-      //if dataToPartition[j] > p, do nothing.  (If new element is greater than pivot)
+  for (let j = l + 1; j <= r; j++) {
+    if (dataToPartition[j] < pivotValue) {
+      //if dataToPartition[j] > pivotValue, do nothing.  (If new element is greater than pivot)
       //Swap dataToPartition[j] and dataToPartition[i]
-      const temp = dataToPartition[i];
-      dataToPartition[j] = dataToPartition[j];
-      dataToPartition[i] = temp;
+      swap(dataToPartition, i, j);
       i = i + 1;
     }
   }
 
-  const temp = dataToPartition[l];
-  dataToPartition[l] = dataToPartition[i - 1];
-  dataToPartition[i - 1] = temp;
+  swap(dataToPartition, l, i - 1);
+  return i - 1;
+};
+
+/**
+ *
+ * @param dataToSort
+ * @param firstIndex
+ * @param secondIndex
+ */
+const swap = function(dataToSort, firstIndex, secondIndex) {
+  const temp = dataToSort[firstIndex];
+  dataToSort[firstIndex] = dataToSort[secondIndex];
+  dataToSort[secondIndex] = temp;
 };
--- algo-I-javascript/test/assignment2_spec.js
+++ algo-I-javascript/test/assignment2_spec.js
@@ -1,13 +1,22 @@
 import {expect} from 'chai';
 import {parseLargeIntegerSetFromDataFile} from './test_helper';
-import {quickSort, pivotAroundFirstElement, pivotAroundLastElement, pivotMedianOfThree} from '../assignments/assignment2';
+import {executeQuickSort, pivotAroundFirstElement, pivotAroundLastElement, pivotMedianOfThree} from '../assignments/assignment2';
 
 describe('compute number of comparisons', () => {
 
   it('pivot around first element using full sample', () => {
     const fullDataArray = parseLargeIntegerSetFromDataFile('./test/assignment2testData.txt');
 
-    const numberOfComparisons = quickSort(fullDataArray, pivotAroundFirstElement);
+    const numberOfComparisons = executeQuickSort(fullDataArray, pivotAroundFirstElement);
+    console.log("sortedData = " + fullDataArray);
+    //const numInversions = calculateInversions(fullDataArray);
+  });
+
+  it('pivot around first element using simple sample', () => {
+    const fullDataArray = [4, 5, 3, 1, 2];
+
+    const numberOfComparisons = executeQuickSort(fullDataArray, pivotAroundFirstElement);
+    console.log("sortedData = " + fullDataArray);
     //const numInversions = calculateInversions(fullDataArray);
   });
 });
