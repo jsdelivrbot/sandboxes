ebdbe15bd373c4f58b1803a0fb685767365bda5d
Dustin Stanley <dustin.stanley@gmail.com>
2016-12-30 02:40:57 -0800

    Add JWT functionality to katharsis-jpa project

--- springboot-katharsis-jpa/src/main/java/com/example/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-//package com.example.dao;
-//
-//import com.example.model.UsersEntity;
-//import org.springframework.data.repository.PagingAndSortingRepository;
-//
-//public interface UserDAO extends PagingAndSortingRepository<UsersEntity, Integer> {
-//
-//}
